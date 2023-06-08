29f101b8357454e17e17af564fd2e00154701377
Hugoren Martinako <aumpfbahn@gmail.com>
2023-06-08 11:01:56 +0200

    Merge branch 'feature/redesign-elections' into feature/redesign-staging

--- decidim-elections/app/packs/entrypoints/decidim_elections.js
+++ decidim-elections/app/packs/entrypoints/decidim_elections.js
@@ -1,2 +1,20 @@
+import "src/decidim/elections/election_log";
+import "src/decidim/elections/trustee/key_ceremony";
+import "src/decidim/elections/trustee/tally";
+import "src/decidim/elections/trustee/trustee_zone";
+
+// REDESIGN_PENDING: setup-vote and setup-preview MUST NOT LOAD at the same time
+// they should be loaded asynchronously depending "preview_mode?" rails variable
+// thourgh "async imports", see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/import
+import "src/decidim/elections/voter/setup-vote";
+import "src/decidim/elections/voter/setup-preview";
+
+import "src/decidim/elections/voter/casting-vote";
+import "src/decidim/elections/voter/new-vote";
+import "src/decidim/elections/voter/verify-vote";
+
 // Images
 require.context("../images", true)
+
+// CSS
+import "stylesheets/decidim/elections/elections.scss"
